ca8e17d46d5f1886117a3f7ff984f1318088769c
Yashi chauhan <yashi.chauhan@digixito.com>
2026-01-21 13:02:29 +0530

    akeit responsive

--- app/users-management/page.tsx
+++ app/users-management/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-
+//
 import UserManagementLayout from "@/components/layouts/userManagementLayout";
 
 export default function UserManagementPage() {
-    return <UserManagementLayout />;
+  return <UserManagementLayout />;
 }
